e831642427fe02630612bec5e3aeb730d11423ab
Lokendra Singh Parmar <Parmar.loken@gmail.com>
2026-09-13 13:24:00 +0530

    File upload issue

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { generateReceiptPDF } = require('./services/pdfService');
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '50mb' }));
 
 app.use(express.static('public'));
 app.use('/css', express.static(path.join(__dirname, 'css')));
